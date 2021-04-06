51154ab1114f612d21372aa1532842d27fbf3f47
guille <guille2358@gmail.com>
2021-04-06 16:11:00 -0300

    update procesamiento

--- procesamiento_datos/parse_data.py
+++ procesamiento_datos/parse_data.py
@@ -1,10 +1,10 @@
 import csv
 
 # Abre el archivo csv fuente
-with open('lote1_serviverde.csv', mode='r') as csvfile:
+with open('serviverde_merge.csv', mode='r') as csvfile:
     reader = csv.DictReader(csvfile)
     # Abre el archvo destino, puede no existir y lo crea
-    with open('sowing_template.csv', mode='w') as writecsv:
+    with open('procesado.csv', mode='w') as writecsv:
         # Headers necesarios para el writer
         fieldnames = ['date', 'hour', 'plot', 'crop', 'unit',
                       'quantity', 'time_to_harvest', 'harvest_duration']
@@ -13,6 +13,6 @@ with open('lote1_serviverde.csv', mode='r') as csvfile:
         writer.writeheader()
         # Escribe por cada fila presente en el csv original
         for row in reader:
-            newRow = {'date': row['FECHA'], 'plot': '1', 'quantity': row['CANTIDAD'],
+            newRow = {'date': row['FECHA'], 'plot': row['LOTE'], 'unit': 'planta', 'quantity': row['CANTIDAD'],
                       'time_to_harvest': row['fecha de cosecha'], 'crop': row['Variedad']}
             writer.writerow(newRow)
